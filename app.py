2657ced6ffe3d57d07c72311edd38ab07a5db15d
vineethgithub2311 <36512635+vineethgithub2311@users.noreply.github.com>
2019-02-11 13:11:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 import numpy as np
-import pandas as pd
 import datetime as dt
 
 
